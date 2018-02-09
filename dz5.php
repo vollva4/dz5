42c98beecb168c9fea2f30efc8b874abd5bf33a1
Your Name <you@example.com>
2018-02-09 17:38:52 +0300

    first commit

---
+++ dz5.php
@@ -0,0 +1,41 @@
+<?php 
+$content = file_get_contents('bd.json') ;
+//echo $content;
+$decoded = json_decode($content, true);
+/*echo '<pre>';
+print_r($decoded);
+echo count($decoded)*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+</head>
+<body>
+    <div>
+    	<table style="border: 1px solid black">
+    		<thead>	Телефонная книга.</thead>
+    		<tr>
+    			<th>Имя.	</th>
+    			<th>Фамилия.	</th>
+    			<th>Адрес.	</th>
+    			<th>Телефон.	</th>
+    		</tr>
+    		<?php
+    		for ($i=0; $i < count($decoded); $i++) {
+    			$name = $decoded[$i] ['firstName'];
+    			$family = $decoded[$i] ['lastName'];
+    			$adress = $decoded[$i] ['address'];
+    			$number = $decoded[$i] ['phoneNumber'];
+    			?>
+    			<tr>
+    			<td><?=$name ?>	</td>
+    			<td><?=$family ?>	</td>
+    			<td><?=$adress ?>	</td>
+    			<td><?=$number ?>	</td>	
+    			</tr>
+    		<?php }; ?>
+    	</table>
+    </div>
+</body>
